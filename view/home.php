5c431229d011c23312fe4afe2745b6c8cfb21aa8
anass0347 <50557143+anass0347@users.noreply.github.com>
2019-12-15 16:09:57 +0100

    Add files via upload
    homepage deel gemaakt

--- view/home.php
+++ view/home.php
@@ -1,9 +1,43 @@
 <?php include 'header.php';?>
 
+<!-- eerste sectie-->
+    <div class="row eerste">
+        <div class="col">
+            <h1>GAMES SHOP</h1>
+            <p>The latest games and consoles</p>
+            <div class="knoppen">
+                <button class="btn links">Bekijk ons aanbod</button>
+                <button class="btn rechts">Neem contact op</button>
+            </div>
+        </div>
+    </div>
 
+<!--  tweede sectie  -->
 <div class="container">
-    <h1>GAMES SHOP</h1>
+    <div class="row tweede">
+        <div class="col-12">
+            <h3>Ons aanbod</h3>
+            <hr>
+        </div>
+        <div class="col-sm-3">
+            <img src="view/assets/images/ps2.jpg" width="100%">
+            <p>ahhaahha</p>
+        </div>
+        <div class="col-sm-3">
+            <img src="view/assets/images/ps3.jpg" width="100%">
+            <p>ahhaahha</p>
+        </div>
+        <div class="col-sm-3">
+            <img src="view/assets/images/ps4.jpg" width="100%">
+            <p>ahhaahha</p>
+        </div>
+        <div class="col-sm-3">
+            <img src="view/assets/images/switchlite.jpg" width="100%">
+            <p>ahhaahha</p>
+        </div>
+    </div>
 </div>
+
 
 
 
