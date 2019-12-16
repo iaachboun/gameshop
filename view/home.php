1c8eae978f2059af45c28dc5d56d99f9ca502544
Chen pei Hu <chenpeihu6@gmail.com>
2019-12-16 16:34:00 +0100

    Nieuwe setie 'over ons' css aangepast, aanbod sectie verandert

--- view/home.php
+++ view/home.php
@@ -1,40 +1,116 @@
 <?php include 'header.php'; ?>
 
     <!-- eerste sectie-->
-    <div class="row eerste">
+    <div class="row intro">
         <div class="col">
             <h1>GAMES SHOP</h1>
             <p>The latest games and consoles</p>
             <div class="knoppen">
-                <button class="btn links">Bekijk ons aanbod</button>
-                <button class="btn rechts">Neem contact op</button>
+                <a class="btn links" href="#aanbod">Bekijk ons aanbod</a>
+                <a class="btn rechts">Neem contact op</a>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container" id="over_ons">
+        <div class="row">
+            <div class="col-12 over_ons-title">
+                <h2>De beste webshop voor games!</h2>
+                <p>Alle games op een plek</p>
+                <hr>
+            </div>
+            <div class="col-md-6 col-sm-12">
+                <h3>Keuze uit meer dan 4000 spellen</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non viverra erat. Ut fringilla
+                    vestibulum euismod. Integer ac varius mi, id tempus tortor. Curabitur at dignissim quam, quis
+                    tristique erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a porta est.
+                    Maecenas nisi leo, blandit ut odio ut, rhoncus finibus eros. Aenean a pellentesque urna. Proin et
+                    turpis vestibulum, dapibus tortor a, sollicitudin massa. Vestibulum ante ipsum primis in faucibus
+                    orci luctus et ultrices posuere cubilia Curae; In dignissim ante dui, id hendrerit dolor blandit
+                    vel. Suspendisse consectetur rutrum urna, sit amet tristique tortor fermentum sed. Curabitur in
+                    massa malesuada, finibus urna et, tincidunt felis. Donec sed egestas velit, id sodales lorem. Donec
+                    porttitor dui sapien, id tristique felis auctor non. Quisque posuere accumsan nunc ac gravida.</p>
+            </div>
+            <div class="col-md-6 col-sm-12">
+                <!--                <img src="https://via.placeholder.com/500x300" alt="test" />-->
+                <div class="test-blok">test misschien foto hier ofzo</div>
             </div>
         </div>
     </div>
 
     <!--  tweede sectie  -->
-    <div class="container">
-        <div class="row tweede">
+    <div id="aanbod" class="container">
+        <div class="row aanbod">
             <div class="col-12">
                 <h3>Ons aanbod</h3>
                 <hr>
             </div>
-            <div class="col-sm-3">
-                <img src="view/assets/images/ps2.jpg" width="100%">
-                <p>ahhaahha</p>
+            <div class="card" style="width: 18rem;">
+                <div class="container item-img-container">
+                    <img src="view/assets/images/ps2.jpg" width="100%">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Ps2</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
             </div>
-            <div class="col-sm-3">
-                <img src="view/assets/images/ps3.jpg" width="100%">
-                <p>ahhaahha</p>
+
+            <div class="card" style="width: 18rem;">
+                <div class="container item-img-container">
+                    <img src="view/assets/images/ps3.jpg" width="100%">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Ps3</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
             </div>
-            <div class="col-sm-3">
-                <img src="view/assets/images/ps4.jpg" width="100%">
-                <p>ahhaahha</p>
+
+            <div class="card" style="width: 18rem;">
+                <div class="container item-img-container">
+                    <img src="view/assets/images/ps4.jpg" width="100%">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Ps4</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
             </div>
-            <div class="col-sm-3">
-                <img src="view/assets/images/switchlite.jpg" width="100%">
-                <p>ahhaahha</p>
+
+            <div class="card" style="width: 18rem;">
+                <div class="container item-img-container">
+                    <img src="view/assets/images/switchlite.jpg" width="100%">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Nintendo Switch</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
             </div>
+
+
+            <!--            <div class="col-sm-3">-->
+            <!--                <img src="view/assets/images/ps2.jpg" width="100%">-->
+            <!--                <p>ahhaahha</p>-->
+            <!--            </div>-->
+            <!--            <div class="col-sm-3">-->
+            <!--                <img src="view/assets/images/ps3.jpg" width="100%">-->
+            <!--                <p>ahhaahha</p>-->
+            <!--            </div>-->
+            <!--            <div class="col-sm-3">-->
+            <!--                <img src="view/assets/images/ps4.jpg" width="100%">-->
+            <!--                <p>ahhaahha</p>-->
+            <!--            </div>-->
+            <!--            <div class="col-sm-3">-->
+            <!--                <img src="view/assets/images/switchlite.jpg" width="100%">-->
+            <!--                <p>ahhaahha</p>-->
+            <!--            </div>-->
         </div>
     </div>
 
