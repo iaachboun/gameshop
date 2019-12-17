262cbb691adf100886781b562de078ce44d8d420
germaintje <menus2011@hotmail.com>
2019-12-17 17:30:01 +0100

    y

--- view/header.php
+++ view/header.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="view/assets/style.css" type="text/css">
+    <link rel="stylesheet" href="view/assets/includes.css" type="text/css">
+    <script src="https://kit.fontawesome.com/662656f14c.js" crossorigin="anonymous"></script>
     <title>Gameshop</title>
 </head>
 <body>
--- view/home.php
+++ view/home.php
@@ -114,5 +114,50 @@
         </div>
     </div>
 
+    <div class="whatgameweb" style="background-color: #29293A">
+    <div class="containe">
+        <h1>What you get with gameweb</h1>
+        <hr>
+
+        <div class="row">
+            <div class="col-12 col-xl-3">
+                <h4 class="yel">100+ pages</h4>
+                <p>Brave Theme is unique in many ways, especially in number of pages and elements that it can offer you. More than 100 pages are included with this template for you to select the best.</p>
+            </div>
+            <div class="col-12 col-xl-3">
+                <h4 class="yel">VISUAL PAGE BUILDER</h4>
+                <p>Novi Builder, our new visual page builder, is included with this template. It allows you to easily customize any page of the template without unnecessary coding.</p>
+            </div>
+            <div class="col-12 col-xl-3">
+                <h4 class="yel">RESPONSIVE DESIGN</h4>
+                <p>If you want your future website to be displayed perfectly on any device, then Brave Theme is certainly your #1 choice! With its full responsiveness, even smartphones can load it.</p>
+            </div>
+            <div class="col-12 col-xl-3">
+                <h4 class="yel">300+ COMPONENTS</h4>
+                <p>Our designers and developers did everything possible to enable you with a mindblowing set of components, which includes forms, galleries, accordions, tabs, popups, and more.</p>
+            </div>
+            <div class="col-12 col-xl-3">
+                <h4 class="yel"><i class="fas fa-money-bill"></i> SAVE YOUR MONEY</h4>
+                <p>Brave Theme allows everyone to save an immense amount of money on buying various templates. When you buy Brave, you get a great multipurpose template at an affordable price.</p>
+            </div>
+            <div class="col-12 col-xl-3">
+                <h4 class="yel"><i class="fas fa-share-alt"></i> SOCIAL WIDGETS</h4>
+                <p>It is important to maintain feedback with your clients and subscribers in various social networks. It can be done easily with our social widgets included in our template.</p>
+            </div>
+            <div class="col-12 col-xl-3">
+                <h4 class="yel"><i class="fas fa-headset"></i> 24/7 SUPPORT</h4>
+                <p>Our qualified and friendly support team is always ready to help you, 24 hours a day, 7 days a week. If you have a question about Brave, don’t hesitate to contact them immediately.</p>
+            </div>
+            <div class="col-12 col-xl-3">
+                <h4 class="yel"><i class="fas fa-palette"></i> COLOR SCHEMES</h4>
+                <p>Our every template comes with a certain colour scheme and Brave has several alternative ones to offer. With their help, you can customize the look of your website.</p>
+            </div>
+
+
+        </div>
+
+        </div>
+    </div>
+
 
 <?php include 'footer.php'; ?>
