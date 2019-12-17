d7629cc048410c6172d33bae317f5a2484098dcb
Chen pei Hu <chenpeihu6@gmail.com>
2019-12-17 17:37:54 +0100

    Jimmy footer ah haha

--- view/footer.php
+++ view/footer.php
@@ -1,2 +1,22 @@
+<footer class="page-footer font-small">
+    <div class='container'>
+        <div class='row'>
+            <div class='col-4'>
+                <h3>GAMEWEB</h3>
+            </div>
+            <div class='col-4'>
+                <p class='footer-copyright text-center py-3'>© 2019 Brave Theme. Privacty Policy</p>
+            </div>
+            <div class='col-4'>
+                <a href="#" class="fa fa-facebook"></a>
+                <a href="#" class="fa fa-twitter"></a>
+                <a href="#" class="fa fa-instagram"></a>
+                <a href="#" class="fa fa-google"></a>
+                <a href="#" class="fa fa-linkedin"></a>
+            </div>
+        </div>
+    </div>
+</footer>
+
 </body>
 </html>
